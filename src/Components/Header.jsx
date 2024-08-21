3c8171f3857c5aa554098984f63d41cc8613549f
Sk. Imran Hussain <skimranpc@gmail.com>
2024-08-21 19:58:00 +0530

    added styling on nav bar and gradient to hero section

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,45 +8,49 @@ import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useLocation } from "react-router-dom";
 
 const TopLayer = () => {
   return (
     <div className="w-full px-2 bg-[#20AD94] py-1  ">
       <div className="sm:w-4/5 w-full m-auto flex justify-between items-center">
-      <div className="flex flex-wrap md:flex-nowrap md:gap-8 gap-2 text-white opacity-80 text-sm">
-        <div className="flex gap-2 items-center ">
-          <FontAwesomeIcon icon={faPhone} className="-scale-x-100" />
-          <p className="text-xs md:text-sm"> +91 83348 35908 </p>
+        <div className="flex flex-wrap md:flex-nowrap md:gap-8 gap-2 text-white opacity-80 text-sm">
+          <div className="flex gap-2 items-center ">
+            <FontAwesomeIcon icon={faPhone} className="-scale-x-100" />
+            <p className="text-xs md:text-sm"> +91 83348 35908 </p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <FontAwesomeIcon icon={faLocationDot} />
+            <p className="text-xs md:text-sm">59C GULAM JILANI KHAN ROAD</p>
+          </div>
         </div>
-        <div className="flex gap-2 items-center">
-          <FontAwesomeIcon icon={faLocationDot} />
-          <p className="text-xs md:text-sm">59C GULAM JILANI KHAN ROAD</p>
+        <div className="text-black text-sm flex gap-2 items-center ">
+          <FontAwesomeIcon
+            icon={faYoutube}
+            className="rounded-full bg-white md:p-3 p-2"
+          />
+          <FontAwesomeIcon
+            icon={faInstagram}
+            className="rounded-full bg-white md:p-3 p-2"
+          />
+          <FontAwesomeIcon
+            icon={faFacebook}
+            className="rounded-full bg-white md:p-3 p-2"
+          />
+          <FontAwesomeIcon
+            icon={faXTwitter}
+            className="rounded-full bg-white md:p-3 p-2"
+          />
         </div>
-      </div>
-      <div className="text-black text-sm flex gap-2 items-center ">
-        <FontAwesomeIcon
-          icon={faYoutube}
-          className="rounded-full bg-white md:p-3 p-2"
-        />
-        <FontAwesomeIcon
-          icon={faInstagram}
-          className="rounded-full bg-white md:p-3 p-2"
-        />
-        <FontAwesomeIcon
-          icon={faFacebook}
-          className="rounded-full bg-white md:p-3 p-2"
-        />
-        <FontAwesomeIcon
-          icon={faXTwitter}
-          className="rounded-full bg-white md:p-3 p-2"
-        />
-      </div>
       </div>
     </div>
   );
 };
 
 const BottomLayer = () => {
+  const { pathname } = useLocation();
+  console.log(pathname);
+
   return (
     <div className="md:w-3/4 w-full m-auto  flex items-center justify-between">
       <div className="p-8 sm:hidden">
@@ -59,14 +63,44 @@ const BottomLayer = () => {
 
       <nav className="sm:block hidden">
         <ul className="flex gap-8 text-base font-semibold uppercase ">
-          <li>
-            <Link to="/">Home</Link>
+          <li className="flex flex-col items-center">
+            <Link
+              to="/"
+              className={`${pathname === "/" ? "text-[#20AD94] transition-colors duration-200 ease-in-out" : ""}`}
+            >
+              Home
+            </Link>
+            <div
+              className={`${pathname === "/" ? "w-12 h-1 bg-[#20AD94] transition-colors duration-500 ease-in-out" : ""}`}
+            ></div>
           </li>
-          <li>
-            <Link to="/about">About Us</Link>
+
+          <li className="flex flex-col items-center">
+            <Link
+              to="/about"
+              className={`${pathname === "/about" ? "text-[#20AD94] transition-colors duration-200 ease-in-out" : ""}`}
+            >
+              About Us
+            </Link>
+            <div
+              className={`${
+                pathname === "/about" ? "w-20 h-1 bg-[#20AD94] transition-colors duration-500 ease-in-out" : ""
+              }`}
+            ></div>
           </li>
-          <li>
-            <Link to="/contact">Contact Us</Link>
+
+          <li className="flex flex-col items-center">
+            <Link
+              to="/contact"
+              className={`${pathname === "/contact" ? "text-[#20AD94] transition-colors duration-200 ease-in-out" : ""}`}
+            >
+              Contact Us
+            </Link>
+            <div
+              className={`${
+                pathname === "/contact" ? "w-24 h-1 bg-[#20AD94] transition-colors duration-500 ease-in-out" : ""
+              }`}
+            ></div>
           </li>
         </ul>
       </nav>
@@ -76,7 +110,7 @@ const BottomLayer = () => {
       <div className="bg-[#FE4C1B] mr-4 md:mr-12 my-2 px-2 py-2 sm:px-6 sm:py-3 flex items-center">
         <a
           href="tel:+918334835908"
-          className=" text-white p text-opacity-80 text-sm"
+          className=" text-white text-sm"
         >
           Join Now{" "}
         </a>
--- src/Components/Sections/Hero.jsx
+++ src/Components/Sections/Hero.jsx
@@ -2,33 +2,43 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
 
-
 const Hero = () => {
   return (
-    <div className="w-screen max-h-fit   bg-gradient-to-tr from-[#d1dcd7] to-[#9a9a9a]">
+    <div className="w-screen h-fit animated-gradient">
       <div className="w-3/4 m-auto flex flex-wrap md:flex-nowrap items-center justify-between">
+        <div className="flex flex-col gap-4 pt-4 md:pt-0">
+          <h2 className="lg:text-5xl  text-3xl font-semibold ">
+            Achieve Excellence in Commerce
+          </h2>
+          <p className="md:w-full opacity-80">
+            Receive tailored support in commerce for Class 11 & 12 and B.Com.
+            Missed a class? Catch up anytime with our extensive YouTube video
+            library.
+          </p>
+          <div className="">
+            <a
+              href="https://www.youtube.com/@knowledgenationkolkata"
+              className="bg-[#20AD94] px-4 py-2 text-white text-opacity-80 text-sm"
+            >
+              <FontAwesomeIcon icon={faExclamationCircle} className=" pr-2" />
+              Learn More
+            </a>
+          </div>
+        </div>
 
-      <div className="flex flex-col gap-4 pt-4 md:pt-0">
-        <h2 className="text-5xl font-semibold ">Achieve Excellence in Commerce</h2>
-        <p className="md:w-2/4 opacity-80">Receive tailored support in commerce for Class 11 & 12 and B.Com. Missed a class? Catch up anytime with our extensive YouTube video library.</p>
-        <div className="">
-        <a href="https://www.youtube.com/@knowledgenationkolkata" className="bg-[#20AD94] px-4 py-2 text-white text-opacity-80 text-sm"><FontAwesomeIcon icon={faExclamationCircle} className=" pr-2" />Learn More</a>
-      </div>
-      </div>
-
-      
-      <div className="overflow-hidden flex justify-end md:h-full h-1/2">
-        <img
-          src="/assets/heroHD.png"
-          className="md:w-96 hidden md:block w-1/2 object-cover pt-4"
-          alt="hero"
-        ></img>
-        <img
-          src="/assets/heroHD_mob.png"
-          className="md:w-96 md:hidden w-1/2 object-cover pt-4"
-          alt="hero"
-        ></img>
-      </div>
+        <div className="overflow-hidden  md:h-full h-1/2 ">
+          <picture className="flex justify-end  ">
+            <source
+              srcSet="/assets/heroHD3.png"
+              media="(min-width: 768px)" // Matches 'md' breakpoint and up
+            />
+            <img
+              src="/assets/heroHD_mob.png"
+              className="w-1/2 object-cover pt-4 "
+              alt="hero"
+            />
+          </picture>
+        </div>
       </div>
     </div>
   );
