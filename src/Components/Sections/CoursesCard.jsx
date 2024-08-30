2a2e486f1b8c1aee1f990f05ea93762066a4ff26
Sk. Imran Hussain <skimranpc@gmail.com>
2024-08-30 20:58:49 +0530

    more fixes

--- src/Components/Sections/CoursesCard.jsx
+++ src/Components/Sections/CoursesCard.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const CoursesCard = ({course}) => {
+const CoursesCard = ({ course }) => {
   return (
-    <div>
-      {course?.items && <div><img src={course?.items[0]?.snippet?.thumbnails?.default?.url} alt="thumbnail"></img></div>}
-    <h2>{course?.courseName}</h2>
-    <p>{course?.courseDesc}</p>
-    <p>{course?.courseTags}</p>
-    </div>
-  )
-}
+    course?.items && (
+      <div>
+        <div>
+          <img
+            src={course?.items[0]?.snippet?.thumbnails?.default?.url}
+            alt="thumbnail"
+          />
+        </div>
+        <h2>{course?.courseName}</h2>
+        <p>{course?.courseDesc}</p>
+        <p>{course?.courseTags}</p>
+      </div>
+    )
+  );
+};
 
-export default CoursesCard
+export default CoursesCard;
